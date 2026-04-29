9818596f0447a112d09936491fbb8e17e91ae586
akila94 <akilaaroshanaa@gmail.com>
2026-04-29 15:27:29 +0530

    Add indexes to db scripts

--- miscellaneous/terminology-service/modules/store_h2/script.sql
+++ miscellaneous/terminology-service/modules/store_h2/script.sql
@@ -80,4 +80,33 @@ CREATE TABLE "valueset_compose_include_value_sets" (
 	PRIMARY KEY("valueSetComposeIncludeValueSetId")
 );
 
+-- CodeSystem/$lookup, CodeSystem/$subsumes, CodeSystem read-by-id, CodeSystem search
+CREATE INDEX "idx_codesystems_id" ON "codesystems"("id");
+CREATE INDEX "idx_codesystems_url" ON "codesystems"("url");
+CREATE INDEX "idx_codesystems_url_version" ON "codesystems"("url", "version");
+CREATE INDEX "idx_codesystems_status" ON "codesystems"("status");
+CREATE INDEX "idx_codesystems_name" ON "codesystems"("name");
 
+-- ValueSet/$expand, ValueSet/$validate-code, ValueSet read-by-id, ValueSet search
+CREATE INDEX "idx_valuesets_id" ON "valuesets"("id");
+CREATE INDEX "idx_valuesets_url" ON "valuesets"("url");
+CREATE INDEX "idx_valuesets_url_version" ON "valuesets"("url", "version");
+CREATE INDEX "idx_valuesets_status" ON "valuesets"("status");
+CREATE INDEX "idx_valuesets_name" ON "valuesets"("name");
+
+-- $lookup, $validate-code, $find-code, $subsumes hierarchy traversal
+CREATE INDEX "idx_concepts_codesystem_id" ON "concepts"("codesystemCodeSystemId");
+CREATE INDEX "idx_concepts_code" ON "concepts"("code");
+CREATE INDEX "idx_concepts_codesystem_code" ON "concepts"("codesystemCodeSystemId", "code");
+CREATE INDEX "idx_concepts_parent" ON "concepts"("parentConceptId");
+CREATE INDEX "idx_concepts_display" ON "concepts"("display");
+
+-- ValueSet/$expand: compose include traversal
+CREATE INDEX "idx_vci_valueset_id" ON "valueset_compose_includes"("valuesetValueSetId");
+CREATE INDEX "idx_vci_codesystem_id" ON "valueset_compose_includes"("codeSystemId");
+
+-- Join tables for $expand
+CREATE INDEX "idx_vcic_compose_id" ON "valueset_compose_include_concepts"("valuesetcomposeValueSetComposeIncludeId");
+CREATE INDEX "idx_vcic_concept_id" ON "valueset_compose_include_concepts"("conceptConceptId");
+CREATE INDEX "idx_vcivs_compose_id" ON "valueset_compose_include_value_sets"("valuesetcomposeValueSetComposeIncludeId");
+CREATE INDEX "idx_vcivs_valueset_id" ON "valueset_compose_include_value_sets"("valuesetValueSetId");
--- miscellaneous/terminology-service/modules/store_pg/script.sql
+++ miscellaneous/terminology-service/modules/store_pg/script.sql
@@ -49,15 +49,6 @@ CREATE TABLE "valueset_compose_includes" (
 	PRIMARY KEY("valueSetComposeIncludeId")
 );
 
-CREATE TABLE "valueset_compose_include_concepts" (
-	"valueSetComposeIncludeConceptId"  SERIAL,
-	"valuesetcomposeValueSetComposeIncludeId" INT NOT NULL,
-	FOREIGN KEY("valuesetcomposeValueSetComposeIncludeId") REFERENCES "valueset_compose_includes"("valueSetComposeIncludeId"),
-	"conceptConceptId" INT NOT NULL,
-	FOREIGN KEY("conceptConceptId") REFERENCES "concepts"("conceptId"),
-	PRIMARY KEY("valueSetComposeIncludeConceptId")
-);
-
 CREATE TABLE "concepts" (
 	"conceptId"  SERIAL,
 	"code" VARCHAR(191) NOT NULL,
@@ -70,6 +61,15 @@ CREATE TABLE "concepts" (
 	PRIMARY KEY("conceptId")
 );
 
+CREATE TABLE "valueset_compose_include_concepts" (
+	"valueSetComposeIncludeConceptId"  SERIAL,
+	"valuesetcomposeValueSetComposeIncludeId" INT NOT NULL,
+	FOREIGN KEY("valuesetcomposeValueSetComposeIncludeId") REFERENCES "valueset_compose_includes"("valueSetComposeIncludeId"),
+	"conceptConceptId" INT NOT NULL,
+	FOREIGN KEY("conceptConceptId") REFERENCES "concepts"("conceptId"),
+	PRIMARY KEY("valueSetComposeIncludeConceptId")
+);
+
 CREATE TABLE "valueset_compose_include_value_sets" (
 	"valueSetComposeIncludeValueSetId"  SERIAL,
 	"valuesetcomposeValueSetComposeIncludeId" INT NOT NULL,
@@ -79,4 +79,33 @@ CREATE TABLE "valueset_compose_include_value_sets" (
 	PRIMARY KEY("valueSetComposeIncludeValueSetId")
 );
 
+-- CodeSystem/$lookup, CodeSystem/$subsumes, CodeSystem read-by-id, CodeSystem search
+CREATE INDEX "idx_codesystems_id" ON "codesystems"("id");
+CREATE INDEX "idx_codesystems_url" ON "codesystems"("url");
+CREATE INDEX "idx_codesystems_url_version" ON "codesystems"("url", "version");
+CREATE INDEX "idx_codesystems_status" ON "codesystems"("status");
+CREATE INDEX "idx_codesystems_name" ON "codesystems"("name");
 
+-- ValueSet/$expand, ValueSet/$validate-code, ValueSet read-by-id, ValueSet search
+CREATE INDEX "idx_valuesets_id" ON "valuesets"("id");
+CREATE INDEX "idx_valuesets_url" ON "valuesets"("url");
+CREATE INDEX "idx_valuesets_url_version" ON "valuesets"("url", "version");
+CREATE INDEX "idx_valuesets_status" ON "valuesets"("status");
+CREATE INDEX "idx_valuesets_name" ON "valuesets"("name");
+
+-- $lookup, $validate-code, $find-code, $subsumes hierarchy traversal
+CREATE INDEX "idx_concepts_codesystem_id" ON "concepts"("codesystemCodeSystemId");
+CREATE INDEX "idx_concepts_code" ON "concepts"("code");
+CREATE INDEX "idx_concepts_codesystem_code" ON "concepts"("codesystemCodeSystemId", "code");
+CREATE INDEX "idx_concepts_parent" ON "concepts"("parentConceptId");
+CREATE INDEX "idx_concepts_display" ON "concepts"("display");
+
+-- ValueSet/$expand: compose include traversal
+CREATE INDEX "idx_vci_valueset_id" ON "valueset_compose_includes"("valuesetValueSetId");
+CREATE INDEX "idx_vci_codesystem_id" ON "valueset_compose_includes"("codeSystemId");
+
+-- Join tables for $expand
+CREATE INDEX "idx_vcic_compose_id" ON "valueset_compose_include_concepts"("valuesetcomposeValueSetComposeIncludeId");
+CREATE INDEX "idx_vcic_concept_id" ON "valueset_compose_include_concepts"("conceptConceptId");
+CREATE INDEX "idx_vcivs_compose_id" ON "valueset_compose_include_value_sets"("valuesetcomposeValueSetComposeIncludeId");
+CREATE INDEX "idx_vcivs_valueset_id" ON "valueset_compose_include_value_sets"("valuesetValueSetId");
